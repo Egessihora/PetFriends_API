d16f7a07c2b99aa642c64590b1c355e6fd2671cf
Anastasiya Begunova <123498835+Egessihora@users.noreply.github.com>
2023-04-04 10:13:18 +0300

    Change email and password

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 
-valid_email = "egessa@yandex.ru"
-valid_password = "egessa1000petfriends"
+valid_email = "Ваш email"
+valid_password = "Ваш password"
 
 invalid_email = "nogessa@yandex.ru"
 invalid_password = "qwerty"
